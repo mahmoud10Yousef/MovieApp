77be9df6c6e3fca9abc17b0a7409128e851b68ae
mahmoud yousef <m.yousef14@yahoo.com>
2023-01-08 15:21:46 -0800

    Initial commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by mahmoud yousef on 08/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Trending movies"
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
